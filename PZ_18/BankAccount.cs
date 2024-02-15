550b105e7725575f5ecdc9cc705d878eec5d12bc
Алексей Неробеев <alex_nerob@mail.ru>
2024-02-15 17:42:18 +0500

    Добавьте файлы проекта.

---
+++ PZ_18/BankAccount.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_18
+{
+    internal class bankAccount
+    {
+        public enum AccountType
+        {
+            Credit,
+            Debit
+        }
+
+        public class BankAccount
+        {
+            private static int debitAccountsCount;
+            private static int creditAccountsCount;
+
+            private decimal balance;
+            private string accountNumber;
+            private AccountType accountType;
+
+            public decimal Balance
+            {
+                get { return balance; }
+            }
+
+            public string AccountNumber
+            {
+                get { return accountNumber; }
+                set
+                {
+                    if (value.Length != 6)
+                    {
+                        Console.WriteLine("Некорректное значение номера счета. Введите 6-значное число.");
+                        return;
+                    }
+
+                    accountNumber = value;
+                }
+            }
+
+            public AccountType AccountType
+            {
+                get { return accountType; }
+                set { accountType = value; }
+            }
+
+            public static int DebitAccountsCount
+            {
+                get { return debitAccountsCount; }
+            }
+
+            public static int CreditAccountsCount
+            {
+                get { return creditAccountsCount; }
+            }
+
+            public BankAccount()
+            {
+                accountType = AccountType.Debit;
+                debitAccountsCount++;
+            }
+
+            public BankAccount(AccountType type)
+            {
+                accountType = type;
+
+                if (type == AccountType.Credit)
+                    creditAccountsCount++;
+                else
+                    debitAccountsCount++;
+            }
+
+            public void Deposit(decimal amount)
+            {
+                if (amount < 1000)
+                {
+                    Console.WriteLine("Сумма пополнения должна быть не менее 1000 рублей.");
+                    return;
+                }
+
+                balance += amount;
+                Console.WriteLine("Счет {0} пополнен на сумму {1}. Баланс: {2}", accountNumber, amount, balance);
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                if (amount > balance)
+                {
+                    Console.WriteLine("Недостаточно средств на счете.");
+                }
+                else if (accountType == AccountType.Credit)
+                {
+                    balance -= amount;
+                    Console.WriteLine("Счет {0} снята сумма {1}. Баланс: {2}", accountNumber, amount, balance);
+                }
+                else if (accountType == AccountType.Debit && amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine("Счет {0} снята сумма {1}. Баланс: {2}", accountNumber, amount, balance);
+                }
+            }
+
+            public static void ShowAccountCounts()
+            {
+                Console.WriteLine("Количество дебетовых счетов: {0}, количество кредитных счетов: {1}",
+                    debitAccountsCount, creditAccountsCount);
+            }
+        }
+    }
+}
---
+++ PZ_18/Program.cs
@@ -0,0 +1,72 @@
+﻿using static PZ_18.bankAccount; //Костылев, Неробеев 2пк2
+
+namespace PZ_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount(AccountType.Debit);
+            account1.AccountNumber = "123456";
+            account1.Deposit(5000);
+            account1.Withdraw(2000);
+            Console.WriteLine("");
+
+            BankAccount account2 = new BankAccount(AccountType.Credit);
+            account2.AccountNumber = "654321";
+            account2.Deposit(3000);
+            account2.Withdraw(5000);
+            Console.WriteLine("");
+
+            BankAccount account3 = new BankAccount();
+            account3.AccountNumber = "123";
+            account3.Deposit(50655);
+            account3.Withdraw(3547);
+            Console.WriteLine("");
+
+            BankAccount account4 = new BankAccount(AccountType.Credit);
+            account4.AccountNumber = "12345765";
+            account4.Deposit(3567);
+            account4.Withdraw(2334);
+            Console.WriteLine("");
+
+            BankAccount account5 = new BankAccount();
+            account5.AccountNumber = "";
+            account5.Deposit(3457568);
+            account5.Withdraw(27654);
+            Console.WriteLine("");
+
+            BankAccount account6 = new BankAccount(AccountType.Debit);
+            account6.AccountNumber = "564324";
+            account6.Deposit(900);
+            account6.Withdraw(100);
+            Console.WriteLine("");
+
+            BankAccount account7 = new BankAccount(AccountType.Credit);
+            account7.AccountNumber = "098476";
+            account7.Deposit(5000);
+            account7.Withdraw(6000);
+            Console.WriteLine("");
+
+            BankAccount account8 = new BankAccount();
+            account8.AccountNumber = "12";
+            account8.Deposit(750);
+            account8.Withdraw(150);
+            Console.WriteLine("");
+
+            BankAccount account9 = new BankAccount();
+            account9.AccountNumber = "12345365";
+            account9.Deposit(399);
+            account9.Withdraw(2000);
+            Console.WriteLine("");
+
+            BankAccount account10 = new BankAccount(AccountType.Credit);
+            account10.AccountNumber = "1234523463";
+            account10.Deposit(5000);
+            account10.Withdraw(3246523);
+            Console.WriteLine("");
+
+            BankAccount.ShowAccountCounts();
+        }
+    }
+}
